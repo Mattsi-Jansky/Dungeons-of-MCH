5431c773fcddac3a44eb1312f12fe0869fea6ba0
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 19:36:51 +0100

    Render a wall tile

--- __init__.py
+++ __init__.py
@@ -2,7 +2,8 @@ import display
 import random
 import machine
 
-from .player import player
+from .assets import player
+from .assets import wall
 
 def drawRandomLine():
   x1 = random.randint(0,320)
@@ -12,7 +13,9 @@ def drawRandomLine():
   color = random.randint(0,0xFFFFFF)
   # display.drawLine(x1,y1,x2,y2,color)
   display.drawFill(0xFFFFFF)
-  display.drawPng(30, 30, player)
+  display.drawPng(32, 32, player)
+  display.drawPng(0, 0, wall)
+  display.drawPng(0, 16, wall)
   display.flush()
   machine.lightsleep(50)
 
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python3 ../mch2022-tools/webusb_fat_push.py __init__.py /flash/apps/python/dungeonsofmch/__init__.py
-python3 ../mch2022-tools/webusb_fat_push.py player.py /flash/apps/python/dungeonsofmch/player.py
+python3 ../mch2022-tools/webusb_fat_push.py assets.py /flash/apps/python/dungeonsofmch/assets.py
