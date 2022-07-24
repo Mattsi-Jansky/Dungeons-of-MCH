b40cf4fcb420b2cb0757db3aae14573411c1632b
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 21:47:12 +0100

    Reorganise refactor
    Hopefully this makes the GC notice that gen is unused

--- __init__.py
+++ __init__.py
@@ -5,18 +5,22 @@ import machine
 from .assets import player, wall, floor
 from .dungeonGenerator import Generator
 
+max_level_size = 64
+map = []
+running = True
+camera = (0,0)
+
 class Tile:
   def __init__(self, tile_type):
     self.tile_type = tile_type
 
-gen = Generator()
-gen.gen_level()
-level = gen.level
-first_room = gen.room_list[0]
-camera = (first_room[0],first_room[1])
-max_level_size = 64
-map = []
-for y in range(len(level)-1):
+def init_map(map,camera):
+  gen = Generator()
+  gen.gen_level()
+  level = gen.level
+  first_room = gen.room_list[0]
+  camera = (first_room[0],first_room[1])
+  for y in range(len(level)-1):
     row = []
     for x in range(len(level[y])-1):
       row.append(Tile(level[x][y]))
@@ -36,6 +40,8 @@ def render():
   display.drawPng(32, 32, player)
   display.flush()
 
-while True:
+init_map(map,camera)
+
+while running:
   render()
   machine.lightsleep(50)
