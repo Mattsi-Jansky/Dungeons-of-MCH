4e397834396eea77160742a944c8a7b8ae795ccb
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 20:15:36 +0100

    Render map

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A game for the MCH2022 badge made in MicroPython.
 
 * Graphics from https://0x72.itch.io/16x16-dungeon-tileset (Thanks Rob!)
 * Dungeon generator (`dungeonGenerator.py`) from James Spencer (Thanks James!)
+  * Source (CC0): http://www.roguebasin.com/index.php/A_Simple_Dungeon_Generator_for_Python_2_or_3
--- __init__.py
+++ __init__.py
@@ -5,16 +5,19 @@ import machine
 from .assets import player, wall
 from .dungeonGenerator import Generator
 
-def render():
+def render(level):
   display.drawFill(0x000000)
+  for x in range(22):
+    for y in range(13):
+      if level[x][y] == 'wall':
+        display.drawPng(x * 16, y * 16, wall)
   display.drawPng(32, 32, player)
-  display.drawPng(0, 0, wall)
-  display.drawPng(0, 16, wall)
   display.flush()
 
 gen = Generator()
 gen.gen_level()
+level = gen.level
 
 while True:
-  render()
+  render(level)
   machine.lightsleep(50)
