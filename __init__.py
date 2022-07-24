b7d3ccafd900f8cb5d9adcc01e3eb6ea42dc7e48
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 22:15:44 +0100

    Render entities

--- __init__.py
+++ __init__.py
@@ -5,10 +5,15 @@ import machine
 from .assets import player, wall, floor
 from .dungeonGenerator import Generator
 
+class Camera:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
 max_level_size = 64
 map = []
 running = True
-camera = (0,0)
+camera = Camera(0,0)
 entities = []
 
 class Entity:
@@ -35,26 +40,28 @@ def init_map(map,camera):
   gen.gen_level()
   level = gen.level
   first_room = gen.room_list[0]
-  camera = (first_room[0],first_room[1])
+  camera.x = first_room[0]
+  camera.y = first_room[1]
   for y in range(len(level)-1):
     row = []
     for x in range(len(level[y])-1):
       row.append(Tile(level[x][y]))
     map.append(row)
-  map[camera[0]][camera[1]].entity = Player()
+  map[camera.x][camera.y].entity = Player()
 
 def render():
   display.drawFill(0x000000)
   for x in range(22):
     for y in range(13):
-      position = (camera[0] + x, camera[1] + y)
+      position = (camera.x + x, camera.y + y)
       if position[0] >= 0 and position[0] < max_level_size \
         and position[1] >= 0 and position[1] < max_level_size:
         if map[position[0]][position[1]].tile_type == 'wall':
           display.drawPng(x * 16, y * 16, wall)
         if map[position[0]][position[1]].tile_type == 'floor':
           display.drawPng(x * 16, y * 16, floor)
-  display.drawPng(32, 32, player)
+        if map[position[0]][position[1]].entity is not None:
+          display.drawPng(x * 16, y * 16, map[position[0]][position[1]].entity.get_graphic())
   display.flush()
 
 init_map(map,camera)
