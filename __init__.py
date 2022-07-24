9cf01466292fe511e9efbba57f32181f9b04c99a
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 20:51:00 +0100

    Add camera support (in theory?)
    Also improved floor sprite

--- __init__.py
+++ __init__.py
@@ -2,22 +2,29 @@ import display
 import random
 import machine
 
-from .assets import player, wall
+from .assets import player, wall, floor
 from .dungeonGenerator import Generator
-
-def render(level):
-  display.drawFill(0x000000)
-  for x in range(22):
-    for y in range(13):
-      if level[x][y] == 'wall':
-        display.drawPng(x * 16, y * 16, wall)
-  display.drawPng(32, 32, player)
-  display.flush()
 
 gen = Generator()
 gen.gen_level()
 level = gen.level
+camera = (0,0)
+max_level_size = 64
+
+def render():
+  display.drawFill(0x000000)
+  for x in range(22):
+    for y in range(13):
+      position = (camera[0] + x, camera[1] + y)
+      if position[0] >= 0 and position[0] < max_level_size \
+        and position[1] >= 0 and position[1] < max_level_size:
+        if level[position[0]][position[1]] == 'wall':
+          display.drawPng(x * 16, y * 16, wall)
+        if level[position[0]][position[1]] == 'floor':
+          display.drawPng(x * 16, y * 16, floor)
+  display.drawPng(32, 32, player)
+  display.flush()
 
 while True:
-  render(level)
+  render()
   machine.lightsleep(50)
