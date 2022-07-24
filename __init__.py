b67ee5c33d5c49689badf8236ff107cf54c85ddc
Mattsi <mattsi.jansky@codurance.com>
2022-07-24 19:51:53 +0100

    Cleanup, improve method naming etc

--- __init__.py
+++ __init__.py
@@ -5,21 +5,16 @@ import machine
 from .assets import player, wall
 from .dungeonGenerator import Generator
 
-def drawRandomLine():
-  x1 = random.randint(0,320)
-  x2 = random.randint(0,320)
-  y1 = random.randint(0,240)
-  y2 = random.randint(0,240)
-  color = random.randint(0,0xFFFFFF)
-  # display.drawLine(x1,y1,x2,y2,color)
-  display.drawFill(0xFFFFFF)
+def render():
+  display.drawFill(0x000000)
   display.drawPng(32, 32, player)
   display.drawPng(0, 0, wall)
   display.drawPng(0, 16, wall)
   display.flush()
-  machine.lightsleep(50)
 
 gen = Generator()
 gen.gen_level()
+
 while True:
-  drawRandomLine()
+  render()
+  machine.lightsleep(50)
